81aa785de8f2220565c4823a75077ac968913164
thevtok <alfarizifikri2406@gmail.com>
2023-08-24 16:54:10 +0700

    chore: add route series

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -36,16 +36,16 @@ router.get('/movies/:id', movieDetails);
 router.get('/movies/:id/streams', streamMovie);
 
 router.get('/genres', setOfGenres);
-router.get('movies/genres/:genre/:page', moviesByGenre);
-router.get('series/genres/:genre/:page', seriesByGenre);
+router.get('/movies/genres/:genre/:page', moviesByGenre);
+router.get('/series/genres/:genre/:page', seriesByGenre);
 
 router.get('/countries', setOfCountries);
-router.get('movies/countries/:country/:page', moviesByCountry);
-router.get('series/countries/:country/:page', seriesByCountry);
+router.get('/movies/countries/:country/:page', moviesByCountry);
+router.get('/series/countries/:country/:page', seriesByCountry);
 
 router.get('/years', setOfYears);
-router.get('movies/years/:year/:page', moviesByYear);
-router.get('series/years/:year/:page', seriesByYear);
+router.get('/movies/years/:year/:page', moviesByYear);
+router.get('/series/years/:year/:page', seriesByYear);
 
 router.get('/series/latest/:page', latestSeries);
 router.get('/popular/series/:page', popularSeries);
